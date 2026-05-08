4e51c21afde827a7e04383ba10230e50ab2b10b5
Alex <alx5409@gmail.com>
2026-05-08 12:46:36 +0200

    Simplify main

--- src/main.py
+++ src/main.py
@@ -2,8 +2,6 @@ from typing import Final
 
 from dotenv import load_dotenv
 
-from cv2.typing import MatLike
-
 from load.image_load import load_images_from_dir
 from view.view_image import show_images
 from logger.logging_config import LoggingConfig
@@ -13,5 +11,5 @@ PNG_IMAGES_DIR: Final[str] = "images"
 if __name__ == "__main__":
     LoggingConfig.configure_logging()
     load_dotenv()
-    images: list[MatLike] = load_images_from_dir(PNG_IMAGES_DIR)
+    images = load_images_from_dir(PNG_IMAGES_DIR)
     show_images(images)
